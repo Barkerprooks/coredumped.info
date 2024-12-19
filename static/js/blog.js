e420b2a308ae514cb648365e623b38d78795c38e
BarkerProoks <jonparkerbrooks@gmail.com>
2024-12-19 00:07:33 -0600

    reorganized and made mobile friendly

---
+++ static/js/blog.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.copy-link-button');
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            buttons.forEach(button => button.innerHTML = 'copy link');
+            navigator.clipboard.writeText(`https://coredumped.info/blog/${button.dataset.post}`);
+            button.innerHTML = 'copied!';
+        });
+    });
+});
