94fd1844efb3a2b8e724d43fa28db465d5853014
Barkerprooks <jonparkerbrooks@gmail.com>
2025-03-28 01:59:38 -0500

    added udptun hosting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 from coredumped import get_banner
@@ -12,3 +12,8 @@ app.register_blueprint(blog)
 @app.get("/")
 async def index():
     return render_template("index.html", banner=await get_banner(), page="home")
+
+
+@app.get("/tools/udptun.py")
+async def udptun_content():
+    return redirect("https://raw.githubusercontent.com/Barkerprooks/udp-tunnel/main/udptun.py")
